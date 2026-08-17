26ad8f2eafaac2843ba36bc0ad321b48bca44373
Yong See <e1525331@u.nus.edu>
2026-08-17 17:49:12 +0800

    Added greetings

--- src/main/java/Friday.java
+++ src/main/java/Friday.java
@@ -3,14 +3,22 @@
  */
 public class Friday {
     public static void main(String[] args) {
+        String separator = "____________________________________________________________";
         String banner = " ______    _     _             \n"
-                + "|  ____|  (_)   | |            \n"
-                + "| |__ _ __ _  __| | __ _ _   _\n"
-                + "|  __| '__| |/ _` |/ _` | | | |\n"
-                + "| |  | |  | | (_| | (_| | |_| |\n"
-                + "|_|  |_|  |_|\\__,_|\\__,_|\\__, |\n"
-                + "                          __/ |\n"
-                + "                         |___/ \n";
-        System.out.println(banner);
+                      + "|  ____|  (_)   | |            \n"
+                      + "| |__ _ __ _  __| | __ _ _   _\n"
+                      + "|  __| '__| |/ _` |/ _` | | | |\n"
+                      + "| |  | |  | | (_| | (_| | |_| |\n"
+                      + "|_|  |_|  |_|\\__,_|\\__,_|\\__, |\n"
+                      + "                          __/ |\n"
+                      + "                         |___/ \n";
+
+        System.out.println(separator);
+        System.out.print(banner);
+        System.out.println("Hello! I'm Friday.");
+        System.out.println("What can I do for you?");
+        System.out.println(separator);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(separator);
     }
 }
