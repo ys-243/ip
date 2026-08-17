e3a98e52dabbf9ee23468fea4accb91c81767621
Yong See <e1525331@u.nus.edu>
2026-08-17 17:32:07 +0800

    changed name to Friday

---
+++ src/main/java/Friday.java
@@ -0,0 +1,16 @@
+/**
+ * Starts the Friday chatbot application.
+ */
+public class Friday {
+    public static void main(String[] args) {
+        String banner = " ______    _     _             \n"
+                + "|  ____|  (_)   | |            \n"
+                + "| |__ _ __ _  __| | __ _ _   _\n"
+                + "|  __| '__| |/ _` |/ _` | | | |\n"
+                + "| |  | |  | | (_| | (_| | |_| |\n"
+                + "|_|  |_|  |_|\\__,_|\\__,_|\\__, |\n"
+                + "                          __/ |\n"
+                + "                         |___/ \n";
+        System.out.println(banner);
+    }
+}
